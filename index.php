bf3479b2b79a71092cf34afc2498374fd51bab18
Divya-ITPN <Divya-ITPN@DESKTOP-EE9KFSF.attlocal.net>
2016-11-08 11:27:44 -0600

    my first commit

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+/*
+ * Created on Nov 8, 2016
+ *
+ * To change the template for this generated file go to
+ * Window - Preferences - PHPeclipse - PHP - Code Templates
+ */
+ 
+echo "My first eclipse and git project";
+?>
